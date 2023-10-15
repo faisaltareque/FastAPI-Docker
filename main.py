4891539caf1b1dd2c74c4d611591bc2476953f3f
Faisal Tareque <39130799+faisaltareque@users.noreply.github.com>
2023-10-15 12:03:52 +0600

    Update main.py

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ def root():
     return {"message": "Hello World"}
 
 @app.post("/message")
-def root(id:str, message:str):
+def message(id:str, message:str):
     return {"id": id, "message": message+f" from id: {id}."}
